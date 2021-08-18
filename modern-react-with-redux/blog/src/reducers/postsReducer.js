94b6187537e261e5ba890beae482d15a851482c3
Arth Limchiu <limchiuarth@gmail.com>
2021-08-18 19:38:34 +0800

    Complete postsReducer

--- modern-react-with-redux/blog/src/reducers/postsReducer.js
+++ modern-react-with-redux/blog/src/reducers/postsReducer.js
@@ -1,5 +1,10 @@
-const postsReducer = () => {
-    return 123;
+const postsReducer = async (state = [], action) => {
+    switch(action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
 };
 
 export default postsReducer;
