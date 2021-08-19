bda828e8bc8cc50b53705291250059703865df0a
Arth Limchiu <limchiuarth@gmail.com>
2021-08-19 20:33:07 +0800

    Initial implementation to try react-router-dom

--- modern-react-with-redux/streams/client/src/components/App.js
+++ modern-react-with-redux/streams/client/src/components/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            Page One
+            <Link to="/pagetwo">Go to page two</Link>
+        </div>
+    );
+};
+
+const PageTwo = () => {
+    return (
+        <div>
+            Page Two
+            <Link to="/">Go to page one</Link>
+        </div>
+    );
+};
 
 const App = () => {
     return (
-        <div>Hello World!</div>
+        <div>
+            <BrowserRouter>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pagetwo" component={PageTwo} />
+            </BrowserRouter>
+        </div>
     );
 };
 
